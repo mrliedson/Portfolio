a5c720d30bd94868a6c13e99de6098be15237be2
mrliedson <alissonbrendo99@gmail.com>
2024-10-18 13:53:56 -0300

    alternei a foto usando toggle, background fixo sem bugs, alterei a cor da fonte do paragrafo do nome de usuario

--- JS/script.js
+++ JS/script.js
@@ -1,15 +1,22 @@
 let toogle = document.querySelector(".toggle");
 let text = document.querySelector(".text");
 let body2 = document.querySelector("body")
+let p = document.querySelector("p, .text")
+let img = document.querySelector("img")
 
 function animatedtoggle(){
     toogle.classList.toggle("active");
     if(toogle.classList.contains("active")){
         text.innerHTML = "ON";
         document.body.style.backgroundImage = "url(../img/bg-desktop.jpg)"
+        p.style.color = 'white';
+        img.src = "img/2.jpg"
+
         
     } else{
         text.innerHTML = "OFF";
         document.body.style.backgroundImage = "url(../img/bg-desktop-light.jpg)"
+        p.style.color = 'black';
+        img.src = "img/1.jpg"
     }
 }
