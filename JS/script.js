f37d6ced71680683efd33e2f110f3211a82470ea
mrliedson <alissonbrendo99@gmail.com>
2024-10-20 15:58:25 -0300

    Apaguei o o fundo preto, corrigi bugs no toggle, e comecei a buscar uma maneira de trocar as cores dos links

--- JS/script.js
+++ JS/script.js
@@ -1,22 +1,22 @@
 let toogle = document.querySelector(".toggle");
-let text = document.querySelector(".text");
+let p = document.querySelector("p");
 let body2 = document.querySelector("body")
-let p = document.querySelector("p, .text")
 let img = document.querySelector("img")
+const link = document.getElementById('a');
+
 
 function animatedtoggle(){
     toogle.classList.toggle("active");
     if(toogle.classList.contains("active")){
-        text.innerHTML = "ON";
         document.body.style.backgroundImage = "url(../img/bg-desktop.jpg)"
-        p.style.color = 'white';
+        link.style.color = `white`;
         img.src = "img/2.jpg"
-
-        
+        p.style.color = 'white';
+       
     } else{
-        text.innerHTML = "OFF";
         document.body.style.backgroundImage = "url(../img/bg-desktop-light.jpg)"
-        p.style.color = 'black';
         img.src = "img/1.jpg"
+        p.style.color = 'black';
+      
     }
 }
